12c8bcab7b689d6298f67c3a5755e311c380bc5b
taylorgigi <ysmgigi@163.com>
2019-10-22 19:59:44 +0800

    add stopwatch util

---
+++ base/stopwatch.h
@@ -0,0 +1,61 @@
+// this utility is used to measure that how long a function or a task spends.
+
+
+
+#ifndef LIBJUPITER_STOPWATCH_INCLUDE_H
+#define LIBJUPITER_STOPWATCH_INCLUDE_H
+
+#include <cstdint>
+#include <chrono>
+
+
+namespace jupiter { // namespace jupiter
+
+
+
+
+class stopwatch
+{
+public:
+    stopwatch(): start_time(std::chrono::time_point::min()) {}
+    ~stopwatch()
+    {
+        start_time = std::chrono::time_point::min();
+    }
+
+    //@return start timing
+    void start()
+    {
+        start_time = std::chrono::steady_clock::now();
+    }
+
+    //@return true if stopwatch has been started, otherwith false
+    bool is_started()
+    {
+        return(start_time == std::chrono::time_point::min());
+    }
+
+    //@return elapsed time(milliseconds) relative to start time
+    uint64_t stop()
+    {
+        if(start_time != std::chrono::time_point::min()) {
+	    std::chrono::time_point now_time = std::chrono::steady_clock::now();
+	    std::chrono::milliseconds t = std::chrono::duration_cast<std::chrono::milliseconds>(now_time - start_time);
+	    start_time = std::chrono::time_point::min();
+	    return t.count();
+	}
+	return 0;
+    }
+private:
+    std::chrono::time_point start_time;
+};
+
+
+
+
+
+} // namespace jupiter
+
+
+#endif
+
